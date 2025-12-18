c415da6b78debc19146153242c1eb93492493f4f
bala-amypo <bala@amypo.in>
2025-12-18 09:23:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
+++ src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
@@ -1,15 +1,10 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.*;
-import java.time.LocalDate;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.IntegrityCase;
 
-public interface IntegrityCaseRepository
-        extends JpaRepository<IntegrityCase, Long> {
+public interface IntegrityCaseRepository extends JpaRepository<IntegrityCase, Long> {
 
-    List<IntegrityCase> findByStudentProfile(StudentProfile s);
-    List<IntegrityCase> findByStudentIdentifier(String id);
-    List<IntegrityCase> findRecentCasesByStatus(String status, LocalDate date);
-    List<IntegrityCase> findByIncidentDateBetween(LocalDate start, LocalDate end);
+    List<IntegrityCase> findByStudentIdentifier(String studentIdentifier);
 }
