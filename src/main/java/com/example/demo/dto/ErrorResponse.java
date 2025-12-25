26e2951a0dd7779443ff0eb27eccdd3fa524920c
bala-amypo <bala@amypo.in>
2025-12-25 10:43:38 +0000

    User commit

--- src/main/java/com/example/demo/dto/ErrorResponse.java
+++ src/main/java/com/example/demo/dto/ErrorResponse.java
@@ -1,17 +1,42 @@
 package com.example.demo.dto;
 
-import lombok.Data;
-import java.time.LocalDateTime;
-
-@Data
 public class ErrorResponse {
-    private LocalDateTime timestamp;
+
     private int status;
     private String error;
     private String message;
     private String path;
-    
-    public ErrorResponse() {
-        this.timestamp = LocalDateTime.now();
+
+    public int getStatus() {
+        return status;
     }
-} 
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
+ 
