c140ef7cb271beb8ac8c4d95ceaf16b06e93dccd
bala-amypo <bala@amypo.in>
2025-12-25 06:22:11 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,20 +1,26 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9230.pro604cr.amypo.ai/")
-                ));
-        }
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Development");
+
+        Info info = new Info()
+                .title("Academic Integrity System API")
+                .version("1.0")
+                .description("This API exposes endpoints for the Academic Integrity Management System.");
+
+        return new OpenAPI().info(info).servers(List.of(server));
+    }
 }
